6cd153d1496740a551bf63c34a2be7b9f6057c9e
chenfang <fxf432@163.com>
2019-10-12 15:27:05 +0800

    10.12

--- src/main/java/com/example/tacocloud/security/SecurityConfig.java
+++ src/main/java/com/example/tacocloud/security/SecurityConfig.java
@@ -12,6 +12,7 @@ import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
+//关于用户的一些常用的查询都会自动生成
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //    No matter which user store you choose, you can configure it by overriding a configure()
 //    method defined in the WebSecurityConfigurerAdapter configuration base class. To
@@ -32,7 +33,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     //将用户信息配置到数据库，进行联动
     protected void configure (AuthenticationManagerBuilder auth) throws Exception{
-        auth.jdbcAuthentication().dataSource(dataSource);
-
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .usersByUsernameQuery(
+                        "select username,password,enabled from Users "+"where username=?")
+                .authoritiesByUsernameQuery("select username,authority from UserAuthorities "+ "where username=?");
+                //说明还有张授权表
+                //团队表 group id,group name, group授权
+                //用户权限表 ：用户名，授权等级
+                //用户个人信息表：用户名，密码，使用状态（是否已经被授权或者其他字段）
     }
 }
