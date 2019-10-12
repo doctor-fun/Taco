545e266455f2b95aa07bdd042eca259608b50f40
fangchen <fxf432@163.com>
2019-10-12 13:32:00 +0800

    10.12

--- src/main/java/com/example/tacocloud/security/SecurityConfig.java
+++ src/main/java/com/example/tacocloud/security/SecurityConfig.java
@@ -1,11 +1,14 @@
 package com.example.tacocloud.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfiguration;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -15,12 +18,21 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 //    get started, you’ll add the following method override to the SecurityConfig class:
 
 
+//    @Override
+//    //在这个类里面指定如何查找用户 配置权限模块
+//    protected  void configure(AuthenticationManagerBuilder auth) throws  Exception{
+//       //在内存内生成用户，写着玩的
+//        auth.inMemoryAuthentication().withUser("buzz").password("infinity").authorities("ROLE_User").and()
+//                .withUser("woody").password("bullseye")
+//                .authorities("ROLE_USER");//授权的角色
+//    }
+    @Autowired
+    DataSource dataSource;
+
     @Override
-    //在这个类里面指定如何查找用户 配置权限模块
-    protected  void configure(AuthenticationManagerBuilder auth) throws  Exception{
-       //在内存内生成用户，写着玩的
-        auth.inMemoryAuthentication().withUser("buzz").password("infinity").authorities("ROLE_User").and()
-                .withUser("woody").password("bullseye")
-                .authorities("ROLE_USER");//授权的角色
+    //将用户信息配置到数据库，进行联动
+    protected void configure (AuthenticationManagerBuilder auth) throws Exception{
+        auth.jdbcAuthentication().dataSource(dataSource);
+
     }
 }
